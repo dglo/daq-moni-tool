891770b147801937e172e9458fdf2e3a20b2085b
Dave Glowacki <dglo@icecube.wisc.edu>
2018-04-26 11:10:42 -0500

    Handle gzipped files

--- src/main/java/icecube/daq/tools/GraphSource.java
+++ src/main/java/icecube/daq/tools/GraphSource.java
@@ -2,10 +2,13 @@ package icecube.daq.tools;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.URL;
+import java.util.zip.GZIPInputStream;
 
 class GraphSource
 {
@@ -25,11 +28,18 @@ class GraphSource
     BufferedReader getReader()
         throws IOException
     {
-        if (file != null) {
-            return new BufferedReader(new FileReader(file));
+        Reader rdr;
+        if (file == null) {
+            rdr = new InputStreamReader(url.openStream());
+        } else if (!file.getName().endsWith(".gz")) {
+            rdr = new FileReader(file);
+        } else {
+            GZIPInputStream gin =
+                new GZIPInputStream(new FileInputStream(file));
+            rdr = new InputStreamReader(gin);
         }
 
-        return new BufferedReader(new InputStreamReader(url.openStream()));
+        return new BufferedReader(rdr);
     }
 
     public String toString()
